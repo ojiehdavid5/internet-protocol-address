8249580cd3753739a190a106cfc5e76e995cd09f
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-29 08:00:14 +0100

    changes

--- server.js
+++ server.js
@@ -13,55 +13,36 @@ const app= express();
 
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.forwardemail.net",
-  port: 465,
-  secure: true,
-  auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
+const html=`<h1>helloworld</h1>   <p>Isn't nodemailer useful</p>`
 
 
-    const getIpAddress = async (req, res, next) => {
-        const ipAddress = req.header('x-forwarded-for') ||
-        req.socket.remoteAddress
-        req.user = ipAddress
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    // from: `"<${"Gideon"}" <${"gideonbabalola69@gmail.com"}>, // sender address
-    from :'"chuks" , "ojiehdavid5@gmail.com"',
-     to:"adavies.ceo@gmail.com", // list of receivers
 
+
+async function main(){
+nodeMailer.createTransport({
+  host:"mail.openjavascript.info",
+  port:465,
+  secure:true,
+  auth:{
+    user:"1@1.com",
+    password:"Nodemailer123!"
+
+
+  }
+  const info= await transporter.sendMail({
     
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+  })
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
+
+})
+
+
+
+
 }
 
-main().catch(console.error)
-
-
-app.get("/" ,(req,res) => {
-  // res.send. json({message:req.user})
-
-  res.send("welcome");
-} );
 
 
 
@@ -73,6 +54,72 @@ app.get("/" ,(req,res) => {
 
 
 
-app.listen(3000 ,() => {
-    console.log('server is running on 3000')
-});}
+
+
+
+
+
+
+
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.forwardemail.net",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+//     user: process.env.EMAIL,
+//     pass: process.env.PASS,
+//   },
+// });
+
+// // async..await is not allowed in global scope, must use a wrapper
+// async function main() {
+
+
+//     const getIpAddress = async (req, res, next) => {
+//         const ipAddress = req.header('x-forwarded-for') ||
+//         req.socket.remoteAddress
+//         req.user = ipAddress
+//   // send mail with defined transport object
+//   const info = await transporter.sendMail({
+//     // from: `"<${"Gideon"}" <${"gideonbabalola69@gmail.com"}>, // sender address
+//     from :'"chuks" , "ojiehdavid5@gmail.com"',
+//      to:"adavies.ceo@gmail.com", // list of receivers
+
+    
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+//   });
+
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//   //
+//   // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+//   //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+//   //       <https://github.com/forwardemail/preview-email>
+//   //
+// }}
+
+// main().catch(console.error)
+
+
+// app.get("/" ,(req,res) => {
+ 
+// }) ;
+
+
+
+
+
+
+
+
+
+
+
+// app.listen(3000 ,() => {
+//     console.log('server is running on 3000');
+// })
